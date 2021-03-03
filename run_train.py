389ee17f23607ea9141e261e786097dd92cf0abf
gitlost-murali <kmanoharmurali@gmail.com>
2021-03-03 17:54:48 +0530

    Giving rank through CUDA_visible_DEVICES and reset local rank to 0

--- run_train.py
+++ run_train.py
@@ -684,7 +684,7 @@ def main():
     parser.add_argument("--local_rank", type=int, default=-1, help="For distributed training: local_rank")
     args = parser.parse_args()
 
-    os.environ["CUDA_VISIBLE_DEVICES"]=args.local_rank
+    os.environ["CUDA_VISIBLE_DEVICES"]=str(args.local_rank)
     args.local_rank = 0
 
     if args.eval_data_file is None and args.do_eval:
